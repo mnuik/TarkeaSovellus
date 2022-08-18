22b8aeb2604ed043283ec944ff22ee09e95a3edb
kaabo-art <kaarlemattila@gmail.com>
2022-08-18 11:55:47 +0300

    Meikän ekat shitit

---
+++ TärkeäSovellus/Models/KaarlenClass.cs
@@ -0,0 +1,11 @@
+﻿namespace TärkeäSovellus.Models
+{
+    public class KaarlenClass
+    {
+        virtual public int Id { get; set; }
+        virtual public string Name { get; set; }
+        public virtual string Description { get; set; }
+
+
+    }
+}
